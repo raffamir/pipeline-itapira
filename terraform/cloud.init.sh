34f2c88bc9847075b48b0fce2c9d50e52e47e29c
raffamir <raffamir@gmail.com>
2026-06-11 11:21:11 -0300

    Instalando Docker 2

--- terraform/cloud.init.sh
+++ terraform/cloud.init.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+# Cria swap de 2GB
+fallocate -l 2G /swapfile
+
+chmod 600 /swapfile
+
+mkswap /swapfile
+
+swapon /swapfile
+
+echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
+
+# Aguarda estabilizar
+sleep 30
+
+# Instala Docker
 dnf install -y docker
 
+# Habilita Docker
 systemctl enable docker
 
 systemctl start docker
 
+# Permissões
 usermod -aG docker opc
 
-mkdir -p /opt/wordpress
+# Evidência
+docker --version > /tmp/docker-version.txt
+
+echo "Provisionamento concluído" > /tmp/provisioning-ok.txt
