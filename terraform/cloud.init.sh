954009bc80777c99c06ef9d694e8073577366352
raffamir <raffamir@gmail.com>
2026-06-11 13:20:57 -0300

    Descobrindo versão ubuntu

--- terraform/cloud.init.sh
+++ terraform/cloud.init.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 fallocate -l 2G /swapfile
-
 chmod 600 /swapfile
-
 mkswap /swapfile
-
 swapon /swapfile
 
 echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
 
-echo "Cloud Init OK" > /tmp/cloud-init-ok.txt
+echo "OK" > /tmp/cloud-init-ok.txt
