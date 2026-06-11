4f26a9d19ec16eebc52ed07fabea4bcd0e117e4e
raffamir <raffamir@gmail.com>
2026-06-11 09:45:47 -0300

    Criando VM OCI - 10

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,53 @@ data "oci_core_images" "oracle_linux" {
   sort_order = "DESC"
 }
 
-output "oracle_images" {
-  value = data.oci_core_images.oracle_linux.images[*].display_name
+resource "oci_core_instance" "wordpress_vm" {
+
+  availability_domain = "XHPb:SA-SAOPAULO-1-AD-1"
+
+  compartment_id = var.compartment_ocid
+
+  display_name = "wordpress-devops-vm"
+
+  shape = "VM.Standard.E2.1.Micro"
+
+  create_vnic_details {
+
+    subnet_id = var.subnet_ocid
+
+    assign_public_ip = true
+
+    display_name = "wordpress-vnic"
+  }
+
+  source_details {
+
+    source_type = "image"
+
+    source_id = data.oci_core_images.oracle_linux.images[1].id
+  }
+
+  metadata = {
+
+    ssh_authorized_keys = var.ssh_public_key
+
+    user_data = base64encode(<<-EOF
+#!/bin/bash
+
+dnf update -y
+
+dnf install -y docker
+
+systemctl enable docker
+
+systemctl start docker
+
+usermod -aG docker opc
+EOF
+    )
+  }
 }
 
-output "oracle_image_ids" {
-  value = data.oci_core_images.oracle_linux.images[*].id
+output "vm_id" {
+  value = oci_core_instance.wordpress_vm.id
 }
