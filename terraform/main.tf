f4fd14bb9bd81e6015d8f7b98dca60a044a04726
raffamir <raffamir@gmail.com>
2026-06-11 09:42:45 -0300

    Criando VM OCI - 9

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,40 +11,10 @@ data "oci_core_images" "oracle_linux" {
   sort_order = "DESC"
 }
 
-resource "oci_core_instance" "wordpress_vm" {
-
-  availability_domain = "XHPb:SA-SAOPAULO-1-AD-1"
-
-  compartment_id = var.compartment_ocid
-
-  display_name = "wordpress-devops-vm"
-
-  shape = "VM.Standard.E2.1.Micro"
-
-  create_vnic_details {
-
-    subnet_id = var.subnet_ocid
-
-    assign_public_ip = true
-
-    display_name = "wordpress-vnic"
-  }
-
-  source_details {
-
-    source_type = "image"
-
-    source_id = data.oci_core_images.oracle_linux.images[0].id
-  }
-
-  metadata = {
-
-    ssh_authorized_keys = var.ssh_public_key
-
-    user_data = base64encode(file("cloud.init.sh"))
-  }
+output "oracle_images" {
+  value = data.oci_core_images.oracle_linux.images[*].display_name
 }
 
-output "vm_id" {
-  value = oci_core_instance.wordpress_vm.id
+output "oracle_image_ids" {
+  value = data.oci_core_images.oracle_linux.images[*].id
 }
